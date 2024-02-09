8a815285190b72688f089841e5c546649d7d9697
puneet-singhal-2408 <49295644+puneet-singhal-2408@users.noreply.github.com>
2024-02-09 11:09:23 +0530

    Update pyramid9.py

--- pyramid9.py
+++ pyramid9.py
@@ -1,3 +1,12 @@
+"""
+Code for below pattern
+        1 
+      2 1 2 
+    3 2 1 2 3 
+  4 3 2 1 2 3 4 
+5 4 3 2 1 2 3 4 5
+"""
+
 def pattern_nine(rows):
     for i in range(1, rows + 1):
         for j in range(rows, 0, -1):
